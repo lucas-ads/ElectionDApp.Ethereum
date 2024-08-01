d3b566e19b79049e7c700913e233d1041d17ae56
Lucas Alves dos Santos <lucas_ads@hotmail.com.br>
2024-08-01 19:16:11 -0400

    Smoke test

---
+++ contracts/Election.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.8.7;
+
+contract Election{
+    
+    //Read/Write candidate
+    string public candidate;
+
+    //Constructor
+    constructor (){
+        candidate = "Candidate 1";
+    }
+
+
+}
